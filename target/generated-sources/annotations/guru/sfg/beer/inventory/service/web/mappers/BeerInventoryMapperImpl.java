be613faebaab07d09e8f2881a1c3e2a0c8f4c7ee
ShigeoShinohara <s.shinohara@sync-web.jp>
2021-10-10 15:15:13 +0900

    mysql error fix

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-10-08T12:05:02+0900",
+    date = "2021-10-10T15:06:35+0900",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.11 (Amazon.com Inc.)"
 )
 @Component
